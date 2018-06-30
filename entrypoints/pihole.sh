957cf7228774bc0efca3d1640151f19c27f35539
Mike Goodwin <xenithorb@users.noreply.github.com>
2018-06-30 15:10:31 -0400

    Update pihole.sh entrypoint to add no-hosts to dnsmasq
    Otherwise `pi.hole` resolves to the docker container's IP address

--- entrypoints/pihole.sh
+++ entrypoints/pihole.sh
@@ -10,8 +10,18 @@ for ip_ref in "${DNS_VARS[@]}"; do
 done
 
 export ServerIP="${ServerIP:-0.0.0.0}"
-
 printf "%s\n" "DNS1: ${DNS1}"  "DNS2: ${DNS2}" "ServerIP: ${ServerIP}"
+
+#Add some stuff to setupVars.conf
+cat <<EOF >> /etc/pihole/setupVars.conf
+TEMPERATUREUNIT=F
+EOF
+
+# Force dnsmasq to not read /etc/hosts, otherwise pi.hole resolves to docker container IP
+cat <<EOF >> /etc/dnsmasq.d/02-no-hosts.conf
+no-hosts
+EOF
+
 
 # Original ENTRYPOINT
 if [[ -f /s6-init ]]; then
